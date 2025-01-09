88b0aa97748e63404a24c45f0a958397d3184895
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-09 18:25:52 -0300

    Refactor Fibonacci check to use readline for user input

--- problem2.ts
+++ problem2.ts
@@ -1,3 +1,5 @@
+import * as readline from 'readline';
+
 function isFibonacci(n: number): boolean {
     let a = 0, b = 1;
     while (b < n) {
@@ -7,12 +9,20 @@ function isFibonacci(n: number): boolean {
 }
 
 function mainFibonacci(): void {
-    const number = parseInt(prompt("Enter a number: ") || "0", 10);
-    if (isFibonacci(number)) {
-        console.log(`The number ${number} belongs to the Fibonacci sequence.`);
-    } else {
-        console.log(`The number ${number} does not belong to the Fibonacci sequence.`);
-    }
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Enter a number: ", (answer) => {
+        const number = parseInt(answer, 10);
+        if (isFibonacci(number)) {
+            console.log(`The number ${number} belongs to the Fibonacci sequence.`);
+        } else {
+            console.log(`The number ${number} does not belong to the Fibonacci sequence.`);
+        }
+        rl.close();
+    });
 }
 
 mainFibonacci();
